e05cb79be0065444af97c68879e9dff11fd4b444
Abenezer Mamo <abmamo@reed.edu>
2018-10-23 01:25:54 +0000

    added script to vectorize puzzles

--- baseline/vectorize.py
+++ baseline/vectorize.py
@@ -6,34 +6,28 @@
 import torch
 import pickle
 
-all_letters = 'abcdefghijklmnopqrstuvwxyz'
+all_letters = '0123456789'
 data = pickle.load( open( "puzzles.pkl", "rb" ) )
 
 def letter_to_index(letter):
     '''
        Function that takes a letter and returns its index in our alphabet
     '''
+    # return the index of the letter in our alphabet
     return all_letters.find(letter)
 
-def letter_to_tensor(letter):
-    '''
-       Take a letter and convert it into a one hot vector
-    '''
-    # initialise an empty tensor
-    tensor = torch.zeros(1, len(all_letters))
-    # use the index of the letter in our alphabet to convert it into a vector
-    tensor[0][letter_to_index(letter)] = 1
-    return tensor 
 
 def line_to_tensor(line):
     '''
        Function to convert an entire string into an n dimensional array where
        each row is a one hot vector representation fo the ith character
     '''
-    # create empty tensor of dimension len(line) x 1 x len(alphabet)
-    tensor = torch.zeros(len(line), 1, len(all_letters))
-    for li, letter in enumerate(line):
-        tensor[li][0][letter_to_index(letter)] = 1
+    # create a 1x10 zero tensor
+    tensor = torch.zeros(1, len(all_letters))
+    for letter in line:
+        # for the ith and jth indices, where i and j are the digits of our
+        # 2 digit number set the value to 1
+        tensor[0][letter_to_index(letter)] = 1
     return tensor
 
 def vectorize_data(data):
@@ -41,8 +35,11 @@ def vectorize_data(data):
        Vectorize generated data
     '''
     vectorized_data = []
+    # for each puzzle generated
     for d in data:
+        # convert everything to vectors
         v_d = {"similar": [line_to_tensor(x) for x in d["similar"]], "odd": [line_to_tensor(x) for x in d["odd"]]}
+        # append to new puzzle list
         vectorized_data.append(v_d)
     return vectorized_data
 
