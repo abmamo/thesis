3cd6c2ce0e3a36a94569579185dc4ba4aaadfea0
Abenezer Mamo <abmamo@reed.edu>
2018-10-23 02:08:24 +0000

    started experimenting with pytorch

--- baseline/nn.py
+++ baseline/nn.py
@@ -1,50 +1,39 @@
+import pickle 
+# get puzzles from pickle file
+data = pickle.load( open( "vectorized_puzzles.pkl", "rb" ) )
+
+import torch
+
+n_letters = 10
+n_hidden = 10
+n_categories = 5
+
 import torch.nn as nn
 
 class RNN(nn.Module):
-      # inherit from the Module class
-      def __init__(self, input_size, hidden_size, output_size):
-          # class initialisation
-          super(RNN, self).__init__()
-      
-          # set hidden layer size
-          self.hidden_size = hidden_size
-          
+    def __init__(self, input_size, hidden_size, output_size):
+        super(RNN, self).__init__()
 
-          # what are these lines?
-          self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
-          self.i2o = nn.Linear(input_size + hidden_size, output_size)
-          self.softmax = nn.LogSoftmax(dim=1)
-     # forward pass
-     def forward(self, input, hidden):
-         # what do these lines do
-         combined = torch.cat((input, hidden), 1)
-         hidden = self.i2h(combined)
-         output = self.i2o(combined)
-         output = self.softmax(output)
-         return output, hidden
+        self.hidden_size = hidden_size
 
-     def init_hidden(self):
-         # return zero matrix
-         return torch.zeros(1, self.hidden_size)
+        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
+        self.i2o = nn.Linear(input_size + hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
 
+    def forward(self, input, hidden):
+        combined = torch.cat((input, hidden), 1)
+        hidden = self.i2h(combined)
+        output = self.i2o(combined)
+        output = self.softmax(output)
+        return output, hidden
 
-criterion = nn.NLLLoss()
+    def initHidden(self):
+        return torch.zeros(1, self.hidden_size)
 
-learning_rate = 0.005 # If you set this too high, it might explode. If too low, it might not learn
+n_hidden = 128
+rnn = RNN(n_letters, n_hidden, n_categories)
 
-def train(category_tensor, line_tensor):
-    hidden = rnn.initHidden()
-
-    rnn.zero_grad()
-
-    for i in range(line_tensor.size()[0]):
-        output, hidden = rnn(line_tensor[i], hidden)
-
-    loss = criterion(output, category_tensor)
-    loss.backward()
-
-    # Add parameters' gradients to their values, multiplied by learning rate
-    for p in rnn.parameters():
-        p.data.add_(-learning_rate, p.grad.data)
-
-    return output, loss.item()
+test_input = torch.cat((data[0]['similar'][0], data[0]['similar'][1], data[0]['similar'][2], data[0]['similar'][3], data[0]['odd'][0]), 0)
+print(test_input)
+#output, next_hidden = rnn(test_input, hidden)
+#print(output)
